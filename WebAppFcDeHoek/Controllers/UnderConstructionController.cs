42dc86d9f8e81cede788aea2645c31f85be4bb3d
Anthony de Prince <Anthony.dePrince@ecs.be>
2018-08-19 17:44:50 +0200

    Create Under Construction Page, edit links

---
+++ WebAppFcDeHoek/Controllers/UnderConstructionController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace WebAppFcDeHoek.Controllers
+{
+    public class UnderConstructionController : Controller
+    {
+        // GET: UnderConstruction
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
